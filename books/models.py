5cc5e4ceb73d7838cc95b4564dcfb49d450198b4
theloop2 <aguileraa835@gmail.com>
2024-03-25 10:11:41 -0400

    first commit (on main >_<)

---
+++ books/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = models.TextField()
+    isbn = models.CharField(max_length=13)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('book-detail', kwargs={'pk': self.pk})
